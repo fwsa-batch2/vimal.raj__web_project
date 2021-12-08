67c4cc4dbf810d57c5b9388d1a3e60a3cfdab463
vimal060 <vimalragu060@gmail.com>
2021-12-08 12:25:36 +0530

    added some files

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,19 +1,38 @@
+let customerlist =[];
+
 function confirm(){
     event.preventDefault();
+    console.log("hello")
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirm_password = document.getElementById("pinput1").value;
+    let mobileNumber = document.getElementById("mnumber").value;
     
-    let password= document.getElementById('pinput').value;
-    let confirm_password= document.getElementById('pinput1').value;
+    const coustomer_details ={
+        "email" : email,
+        "password" : password,
+        "confirm pasword" : confirm_password,
+        "Mobile Number" : mobileNumber,
+        
+        }
 
+    
     if(password != confirm_password){
         alert('password incorrect');
+        let detail = JSON.stringify(coustomer_details);
+        customerlist.push= detail;
+        localStorage. setItem("coustomer_details", detail);
        
-
-
+        
+       
     }
 
     else{
-        window.location.href ="./index.html";
-        
+       
+        window.location.href ="../index.html";  
 
     }
+
 }
+
+
