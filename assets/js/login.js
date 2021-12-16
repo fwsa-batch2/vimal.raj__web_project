bc15f758918f6954014f00cd25f112b17859965d
vimal060 <vimalragu060@gmail.com>
2021-12-16 12:08:26 +0530

    updated js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,50 @@
-function confirm(){
+function confirm() {
   event.preventDefault();
-    let array = JSON.parse(localStorage.getItem("user_details"));
-    let mailid = document.getElementById("email").value;
-    let passwords = document.getElementById("password").value;
-    for(let i=0; i < array.length; i++){
-      if(array[i].email !== mailid && array[i].password !== passwords ){
-         alert("Email or password");
-         break
-      }
-      else{
-        window.open("../index.html");
-      }
+  let array = JSON.parse(localStorage.getItem("user_details"));
+  let mailid = document.getElementById("email").value;
+  let confirm_password = document.getElementById("password").value;
+
+  console.table(array);
+  console.log(array.length);
+  let len = array.length;
+  console.log(len);
+
+
+  for (let i = 0; i < len; i++) {
+    console.log(array[i]);
+    console.log(array[i].email)
+    if (array[i].email == mailid && array[i].password == confirm_password) {
+      window.location.href = "../index.html";
+
+      break;
+      
+      
+      
     }
-   }
 
+    if (array[i].email != mailid && array[i].password != confirm_password) {
+      alert("Email or password");
 
-   function check(){
-       const checkbox = document.getElementById("checkbox");
-       if(checkbox.checked){
-           document.getElementById("password").type ="text";
-           
+      break;
+      
+      
 
-       }
-       else
-       document.getElementById("password").type = "password";
     }
+      
+  }
+}
+
+
+function check() {
+  const checkbox = document.getElementById("checkbox");
+  if (checkbox.checked) {
+    document.getElementById("password").type = "text";
+
+
+  }
+  else
+    document.getElementById("password").type = "password";
+}
 
 
     // email validation
