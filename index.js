8826d8bc21301836dc831a22ceca953b923ad4e2
vimal060 <vimalragu060@gmail.com>
2022-01-03 17:25:41 +0530

    updated js

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 let user_list = [];
 
 function pageOnLoad() {
-
 }
-
-// function addfood(vimal) {
-
-//     let len = vimal.length;
-
-
-
-// }
-
 let menulist = localStorage.getItem("user_details");
-
-// addfood(menulist);
-
 function addmenu() {
     let getvalue = JSON.parse(localStorage.getItem("menu_added"));
-
     let menuname = "";
-
-
     for (let i = 0; i < getvalue.length; i++) {
         let foodname = getvalue[i];
         console.log(foodname);
-        let menudiv = "<div class='menu'><div class='img'><img src='" + foodname.image + "'height='100%' alt='food name'></div><div class='name_div'><div class='name'><h3>" + foodname.shopname + "</h3><button>Add to cart</button></div></div><div class='dish_name'><div><h3>" + foodname.foodnme + "</h3><h3>$" + foodname.prize + "</h3></div></div></div></div>"
+        let menudiv = "<div class='menu'><div class='img'><img class='imageid' src='" + foodname.image + "'height='100%' alt='food name'></div><div class='name_div'><div class='name'><h3 class='shopname'>" + foodname.shopname + "</h3><button class='addcartbtn'>Add to cart</button></div></div><div class='dish_name'><div><h3 class='foodname'>" + foodname.foodnme + "</h3><h3 class='price'>$" + foodname.prize + "</h3></div></div></div></div>"
 
         menuname = menuname + menudiv;
     }
 
     document.getElementById("menu_div").innerHTML = menuname;
-
-
-
 }
+
+// function cart(){
+//     event.preventDefault();
+    
+//     let image_link = document.getElementById("imageid").src;
+//     let shopname = document.getElementById("shopname").innerText;
+//     let foodname= document.getElementById("foodname").innerText;
+//     let prize= document.getElementById("price").innerText;
+    
+    
+
+ 
+        
+// } 
+
 
 addmenu()
 
