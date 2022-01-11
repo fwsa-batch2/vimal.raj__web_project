37802712a41c9541e5ee57ec53af52cc255f6c43
vimal060 <vimalragu060@gmail.com>
2022-01-11 13:06:52 +0530

    updated html

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function addmenu() {
     let menuname = "";
     for (let i = 0; i < getvalue.length; i++) {
         let foodname = getvalue[i];
-        console.log(foodname);
-        let menudiv = "<div class='menu'><div class='img'><img class='imageid' src='" + foodname.image + "'height='100%' alt='food name'></div><div class='name_div'><div class='name'><h3 class='shopname'>" + foodname.shopname + "</h3><button class='addcartbtn'>Add to cart</button></div></div><div class='dish_name'><div><h3 class='foodname'>" + foodname.foodnme + "</h3><h3 class='price'>$" + foodname.prize + "</h3></div></div></div></div>"
+        let menudiv = "<div class='menu'><div class='img'><img class='imageid' src='" + foodname.image + "'height='100%' alt='food name'></div><div class='name_div'><div class='name'><h3 class='shopname'>" + foodname.shopname + "</h3><button class='addcartbtn'>Add to cart</button></div></div><div class='dish_name'><div><h3 class='foodname'>" + foodname.foodname + "</h3><h3 class='price'>$" + foodname.prize + "</h3></div></div></div></div>"
 
         menuname = menuname + menudiv;
     }
