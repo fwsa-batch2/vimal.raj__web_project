6bc0645bf728ce3a157a98fe0b91c2e878f79bc3
vimal060 <vimalragu060@gmail.com>
2022-01-21 16:04:36 +0530

    updated js

---
+++ assets/js/review.js
@@ -0,0 +1,45 @@
+let review_list = [];
+
+function pageOnLoad() {
+    const call = JSON.parse(localStorage.getItem("review_members"))
+    if (call != null) {
+        review_list = call;
+
+    }
+
+}
+
+function review(event) {
+    event.preventDefault();
+    let name = document.getElementById("review_name").value;
+    let city = document.getElementById("city").value;
+    let review = document.getElementById("textarea").value;
+
+    const review_details = {
+        "name": name,
+        "city": city,
+        "review": review,
+
+
+    }
+
+    review_list.push(review_details);
+    localStorage.setItem("review_members", JSON.stringify(review_list));
+
+}
+
+function addmenu() {
+    let getvalue = JSON.parse(localStorage.getItem("review_members"));
+    let menuname = "";
+    let lens = getvalue.length;
+    for (let i = 0; i < lens; i++) {
+        let review = getvalue[i];
+        let menudiv = `<div id="div1"><br><br><img src="../assets/image/IMG_20211005_182554 (1).jpg" alt="" class="avatar"><label id="lvimal" for="vimal"><strong>${review.name} -</strong></label><label id="lvimal1" for="vimal"><strong>${review.city}</strong></label><br><br><p class="p">${review.review}</p><br><img src="../assets/image/pngfind.com-rating-png-transparent-6687141.png" alt="" class="rating"><img src="../assets/image/430-4304893_png-file-svg-comment-icon-png-transparent-png.png" alt="" id="icons"><label class="comment" for="comment">Comment</label></div><br><br><br></div>`
+        menuname = menuname + menudiv;
+    }
+
+    document.getElementById("review-div").innerHTML = menuname;
+}
+
+addmenu()
+pageOnLoad()
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function addmenu() {
         let foodname = getvalue[i];
         let menudiv = "<div class='menu'><div class='img'><img class='imageid' src='" + foodname.image + "'height='100%' alt='food name'></div><div class='name_div'><div class='name'><h3 class='shopname'>" + foodname.shopname + "</h3><button class='addcartbtn'>Add to cart</button></div></div><div class='dish_name'><div><h3 class='foodname'>" + foodname.foodname + "</h3><h3 class='price'>$" + foodname.prize + "</h3></div></div></div></div>"
 
-        menuname = menuname + menudiv;
+        menuname = menuname + menudiv; 
     }
 
     document.getElementById("menu_div").innerHTML = menuname;
