bf0da042e8cd917b23c97f168fc82e777fe9a0bf
vimal060 <vimalragu060@gmail.com>
2022-01-23 22:45:34 +0530

    modified js

--- assets/js/review.js
+++ assets/js/review.js
@@ -33,7 +33,7 @@ function addmenu() {
     let menuname = "";
     let lens = getvalue.length;
     for (let i = 0; i < lens; i++) {
-        let Review = getvalue[i];
+        let review = getvalue[i];
         let menudiv = `<div id="div1"><br><br><img src="../assets/image/IMG_20211005_182554 (1).jpg" alt="" class="avatar"><label id="lvimal" for="vimal"><strong>${review.name} -</strong></label><label id="lvimal1" for="vimal"><strong>${review.city}</strong></label><br><br><p class="p">${review.review}</p><br><img src="../assets/image/pngfind.com-rating-png-transparent-6687141.png" alt="" class="rating"><img src="../assets/image/430-4304893_png-file-svg-comment-icon-png-transparent-png.png" alt="" id="icons"><label class="comment" for="comment">Comment</label></div><br><br><br></div>`
         menuname = menuname + menudiv;
     }
